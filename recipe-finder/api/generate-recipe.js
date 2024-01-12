bc7de039f66b083ab39eb180eb91364dcfc9cff7
Iulian <iuliancuculici@gmail.com>
2024-01-12 15:39:20 +0200

    activate cors8

--- recipe-finder/api/generate-recipe.js
+++ recipe-finder/api/generate-recipe.js
@@ -1,32 +1,11 @@
+// api/generate-recipe.js
+
 const axios = require('axios');
 
 const OPENAI_API_URL =
   'https://api.openai.com/v1/engines/gpt-3.5-turbo-instruct/completions';
 
-const allowCors = (fn) => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true);
-  res.setHeader(
-    'Access-Control-Allow-Origin',
-    'https://personal-blond-two.vercel.app/',
-  ); // sau specifică domeniul tău
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET,OPTIONS,PATCH,DELETE,POST,PUT',
-  );
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
-  );
-
-  if (req.method === 'OPTIONS') {
-    res.status(200).end();
-    return;
-  }
-
-  return await fn(req, res);
-};
-
-const handler = async (req, res) => {
+module.exports = async (req, res) => {
   if (req.method === 'POST') {
     const ingredients = req.body.ingredients;
     const prompt =
@@ -55,5 +34,3 @@ const handler = async (req, res) => {
     res.status(405).send('Metodă nepermisă');
   }
 };
-
-module.exports = allowCors(handler);
