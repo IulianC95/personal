d4524901b79d848eff482cd6915a6c58bcfd3064
Iulian <iuliancuculici@gmail.com>
2024-01-12 14:37:01 +0200

    activate cors

--- recipe-finder/api/generate-recipe.js
+++ recipe-finder/api/generate-recipe.js
@@ -1,16 +1,15 @@
-// api/generate-recipe.js
-res.setHeader('Access-Control-Allow-Origin', '*'); // or specify your front-end domain instead of '*'
-res.setHeader(
-  'Access-Control-Allow-Headers',
-  'Origin, X-Requested-With, Content-Type, Accept',
-);
-
 const axios = require('axios');
 
 const OPENAI_API_URL =
   'https://api.openai.com/v1/engines/gpt-3.5-turbo-instruct/completions';
 
 module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept',
+  );
+
   if (req.method === 'POST') {
     const ingredients = req.body.ingredients;
     const prompt =
